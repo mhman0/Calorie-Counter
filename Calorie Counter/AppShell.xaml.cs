1b9e2bf5f6d1feaab5bcfbc1b0d633c9336dbb32
MH <mike.hall.0316@gmail.com>
2024-01-22 15:00:35 -0600

    Add project files.

---
+++ Calorie Counter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Calorie_Counter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Calorie Counter/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Calorie_Counter
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
