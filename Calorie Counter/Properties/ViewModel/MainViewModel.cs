fcfc5b57dc1374e6eb8a4f67dae59520f942d45b
MH <mike.hall.0316@gmail.com>
2024-01-28 17:30:04 -0600

    Trying to set text from entry field in mainpage

---
+++ Calorie Counter/Properties/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Calorie_Counter.Properties.ViewModel;
+
+//Implimenting MVVM
+public class MainViewModel : INotifyPropertyChanged // going to give us this system command compnent model
+{
+    string text;
+    public string Text
+    {
+        get => text; 
+        set
+        {
+            text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+    void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
+
