2de99813cd8c1060d82a4b356eae49fa9d8bf48a
MH <mike.hall.0316@gmail.com>
2024-01-28 17:39:03 -0600

    tring to get community toolkit for mvvm running

--- Calorie Counter/Properties/ViewModel/MainViewModel.cs
+++ Calorie Counter/Properties/ViewModel/MainViewModel.cs
@@ -1,22 +1,39 @@
-﻿using System.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
 
 namespace Calorie_Counter.Properties.ViewModel;
 
 //Implimenting MVVM
-public class MainViewModel : INotifyPropertyChanged // going to give us this system command compnent model
-{
-    string text;
-    public string Text
-    {
-        get => text; 
-        set
-        {
-            text = value;
-            OnPropertyChanged(nameof(Text));
-        }
-    }
-    public event PropertyChangedEventHandler PropertyChanged;
-    void OnPropertyChanged(string name) =>
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-}
+//// Define a class named MainViewModel that implements the INotifyPropertyChanged interface.
+//public class MainViewModel : INotifyPropertyChanged
+//{
+//    // Declare a private field to store the text value.
+//    string text;
 
+//    // Define a public property named Text that gets and sets the value of the text field.
+//    public string Text
+//    {
+//        get => text; // Getter returns the current value of the text field.
+//        set
+//        {
+//            text = value; // Setter sets the value of the text field to the provided value.
+
+//            // Invoke the OnPropertyChanged method to notify subscribers about the property change.
+//            OnPropertyChanged(nameof(Text));
+//        }
+//    }
+
+//    // Declare an event of type PropertyChangedEventHandler, which is raised when a property value changes.
+//    public event PropertyChangedEventHandler PropertyChanged;
+
+//    // Define a method named OnPropertyChanged that takes the name of the changed property.
+//    void OnPropertyChanged(string name) =>
+//        // Invoke the PropertyChanged event, passing this instance and PropertyChangedEventArgs with the property name.
+//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+//}
+
+public partial class MainViewModel : ObservableObject // 
+{
+    [ObservableProperty]
+    string text;
+}
